5a0092ade36943ed86d8045c72643b951ce99bb4
Ishan Moykhede <moykhedeishan@gmail.com>
2026-04-15 23:11:03 +0530

    frontend deployed

--- server/server.js
+++ server/server.js
@@ -16,13 +16,14 @@ const app = express();
 await connectDb();
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-    exposedHeaders: ["X-Encrypted-IV", "X-File-Name", "X-Mime-Type"],
-  }),
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://vaultvani-frontend.onrender.com"
+  ],
+  credentials: true,
+  exposedHeaders: ["X-Encrypted-IV", "X-File-Name", "X-Mime-Type"],
+}));
 
 app.use(express.urlencoded({ extended: false }));
 
